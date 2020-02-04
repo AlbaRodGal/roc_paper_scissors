3e0988f5bee2cce12689e4e39184a4d85b613bf0
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-02-04 17:22:55 +0000

    Input validation

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Welcome to Rock, Paper, Scissors!")
+console.log("Welcome to Rock, Paper, Scissors!");
+
+const choices = ['rock', 'paper', 'scissors']
+
+// Obtain a choice from the user
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function handleUserResponse (response) {
+
+    // Strip/Ignore capitalisation 
+    response = response.toLowerCase();
+    
+    // Validate user's choice and tell them if they made a mistake
+
+    if (response != 'rock', 'paper', 'scissors') {
+    console.log("Sorry, that's invalid. Please choose either rock, paper or scissors!");
+}
+
+// Make a random selection from the computer
+
+randomChoice = choices.random()
+console.log(randomChoice)
+    // A random choice of Rock, Paper, Scissors
+// Compare the two choices
+
+//Report on who won
+    console.log(response);
+    readline.close();
+}
+readline.question("Please choose either rock, paper or scissors!\n", handleUserResponse)
