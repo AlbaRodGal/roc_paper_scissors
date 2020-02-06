608c757c110589827c46aa37431013ba982094c3
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-02-06 23:19:58 +0000

    user's input validation with match and REGEX

--- index.js
+++ index.js
@@ -7,29 +7,21 @@ const readline = require('readline').createInterface({
     output: process.stdout
 });
 
-function isInputValid (response){
-    //Validate the user's choice and tell them if they if they made a mistake
-    const choices = ['rock', 'paper', 'scissors'];
-
-    response = response.toLowerCase();
+function handleUserResponse (response) {
+//Strip capitalisation/ignore capitalisation of words
     
-    if (response in choices) {
-        return true;
-    } else{
-        return false;
-    }
-}
-
-    //Strip capitalisation/ignore capitalisation of words
-
-    //Make a random selection for the computer
+    const choices = ['rock', 'paper', 'scissors'];
+    response = response.toLowerCase();
+    response.match(/rock|paper|scissors/g))
     //A random choice of Rock, Paper or Scissors
+    let rightAnswer = Math.floor(Math.random()*choices.length);
+    console.log(rightAnswer);
+    //Validate the user's choice and tell them if they if they made a mistake
 
     //Compare the two choices
-
+ 
     //Report on who won
-//     console.log(response);
-//     readline.close();
-// }
-
-readline.question("Please choose either rock, paper or scissors!\n", handleUserReponse);
+    console.log(response);
+    readline.close();
+}
+readline.question("Please choose either rock, paper or scissors!\n", handleUserResponse)
